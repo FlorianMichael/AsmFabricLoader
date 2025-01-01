5abbce9527c838399037ac0dd472b75b9b49036c
FlorianMichael <florian.michael07@gmail.com>
2025-01-01 01:07:55 +0100

    Auto: 2025

--- src/main/java/de/florianmichael/asmfabricloader/api/DummyMixinPlugin.java
+++ src/main/java/de/florianmichael/asmfabricloader/api/DummyMixinPlugin.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of AsmFabricLoader - https://github.com/FlorianMichael/AsmFabricLoader
- * Copyright (C) 2023-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com>
+ * Copyright (C) 2023-2025 FlorianMichael/EnZaXD <florian.michael07@gmail.com>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
