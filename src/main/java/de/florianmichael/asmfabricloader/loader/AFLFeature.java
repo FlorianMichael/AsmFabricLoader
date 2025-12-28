0a92e0eafbd78d636ccf8e3d53a14c60d6660527
FlorianMichael <git@florianmichael.de>
2025-12-28 23:46:53 +0100

    AsmFabricLoader 2.0.2: Prepare for next year (2026)

--- src/main/java/de/florianmichael/asmfabricloader/loader/AFLFeature.java
+++ src/main/java/de/florianmichael/asmfabricloader/loader/AFLFeature.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of AsmFabricLoader - https://github.com/FlorianMichael/AsmFabricLoader
- * Copyright (C) 2023-2025 FlorianMichael/EnZaXD <florian.michael07@gmail.com>
+ * Copyright (C) 2023-2026 FlorianMichael/EnZaXD <git@florianmichael.de>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
